95f5b1fd0a268f1f18f63d2f174352e288806d75
Georgi Georgiev <gatanasovgeorgiev@gmail.com>
2026-09-10 12:37:19 +0300

    fix(fish): persist Catppuccin Latte syntax colors

---
+++ common/.config/fish/conf.d/colors.fish
@@ -0,0 +1,24 @@
+# Catppuccin Latte with Lavender accents.
+set -g fish_color_normal 4c4f69
+set -g fish_color_command 1e66f5
+set -g fish_color_comment 9ca0b0
+set -g fish_color_end fe640b
+set -g fish_color_error d20f39 --bold
+set -g fish_color_escape fe640b
+set -g fish_color_keyword 7287fd
+set -g fish_color_operator 7287fd
+set -g fish_color_option 179299
+set -g fish_color_param 4c4f69
+set -g fish_color_quote 40a02b
+set -g fish_color_redirection ea76cb
+set -g fish_color_search_match --background=ccd0da
+set -g fish_color_selection --background=bcc0cc
+set -g fish_color_valid_path 1e66f5 --underline
+set -g fish_color_autosuggestion acb0be
+set -g fish_pager_color_completion 4c4f69
+set -g fish_pager_color_description 7c7f93
+set -g fish_pager_color_prefix 7287fd --bold
+set -g fish_pager_color_progress 8c8fa1
+set -g fish_pager_color_selected_background --background=bcc0cc
+set -g fish_pager_color_selected_completion 4c4f69
+set -g fish_pager_color_selected_description 5c5f77
